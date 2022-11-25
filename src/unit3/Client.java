91c6f8ba35ece4f71a71a2c9444e59a50c9d06b2
AAusa <95350078+AAusa@users.noreply.github.com>
2022-11-25 11:15:46 +0100

    clientServer

---
+++ src/unit3/Client.java
@@ -0,0 +1,31 @@
+package unit3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        String exit = "";
+        Scanner sc = new Scanner(System.in);
+        try {
+            Socket socketCliente = new Socket("localhost", 7000);
+            PrintWriter output = new PrintWriter(socketCliente.getOutputStream(),true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+            String message = "";
+            while(!message.equalsIgnoreCase("exit")) {
+                System.out.println("Introduce a message:");
+                message = sc.nextLine();
+                output.println(message);
+                System.out.println("The server answers: "+input.readLine());
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
